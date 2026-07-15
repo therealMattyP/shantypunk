051f850bf79fdddb2399094fc3ed420a1516ff16
therealMattyP <therealMattyP@users.noreply.github.com>
2026-07-15 13:42:44 -0400

    Add Stockyard v2 addon registration

---
+++ stockyard_v2/__init__.py
@@ -0,0 +1,32 @@
+bl_info = {
+    "name": "Shantypunk Stockyard v2",
+    "author": "Matt / Shantypunk",
+    "version": (0, 1, 0),
+    "blender": (4, 3, 0),
+    "location": "View3D > Sidebar > Shantypunk",
+    "description": "Generate deterministic, construction-plausible timber stockyard platforms",
+    "category": "Add Mesh",
+}
+
+from .stockyard_v2 import CLASSES
+
+
+def register():
+    import bpy
+
+    for cls in CLASSES:
+        bpy.utils.register_class(cls)
+
+    bpy.types.Scene.stockyard_v2 = bpy.props.PointerProperty(
+        type=next(cls for cls in CLASSES if cls.__name__ == "StockyardV2Properties")
+    )
+
+
+def unregister():
+    import bpy
+
+    if hasattr(bpy.types.Scene, "stockyard_v2"):
+        del bpy.types.Scene.stockyard_v2
+
+    for cls in reversed(CLASSES):
+        bpy.utils.unregister_class(cls)
